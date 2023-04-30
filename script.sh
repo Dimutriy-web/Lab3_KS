da4638d86ca5a16d7a96c9ac6cc239ea78bf7b1f
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-04-30 21:47:19 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 cd /home/grid/testbed/tb325/Lab/vec_samples/src 
 ml icc
 arr=( "-xSSE2" "-xSSE3" "-xAVX"  )
+cat /proc/cpuinfo | grep flag | head -1
 for j in "${arr[@]}"; do
   for i in {1..3}; do
     x1=$(date +%M)
