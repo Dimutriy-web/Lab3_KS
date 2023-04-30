8f650da966a210bf20a63f88ecb94be2f842c290
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-05-01 02:40:43 +0300

    Add files via upload

---
+++ LAB3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+
+int main() {
+    for (int i = 0; i < 1099999; i++) { 
+        int arr[100];
+        for (int j = 0; j < 100; j++) {
+            arr[j] = rand() % 100 + 1; 
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        bubbleSort(arr, n);
+    }
+    cout << "OK";
+    return 0;
+}
