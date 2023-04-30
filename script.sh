bd07917deedb8d6247a7defe22e9d9d94fb4d73d
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-04-30 19:58:25 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/grid/testbed/tb325/Lab/vec_samples/src 
 ml icc
-arr=( "-xSSE2" "-xSSE3" "-xAVX" "-O1" "-O2" "-O3" )
+arr=( "-xSSE2" "-xSSE3" "-xAVX"  )
 for j in "${arr[@]}"; do
   for i in {1..3}; do
     x1=$(date +%M)
