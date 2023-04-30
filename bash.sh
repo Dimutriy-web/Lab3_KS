814e5ba4e557a2cb7c09dd5642c8f5e5e36a5267
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-04-30 15:38:06 +0300

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-grep 'flag' /proc/cpuinfo | head -n 1
-
-for i in '-xSSE2' '-xSSE3' '-xAVX' '-O1' '-O2' '-O3'; do
-    cmd="icpc $i -std=c++17 src/LAB3.cpp -o test"
-    echo $cmd
-    eval $cmd
-    time ./test
-done
