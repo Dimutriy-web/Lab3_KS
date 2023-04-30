6451c5bb6afbcc33fa83a8e9f10f5007e2ecb9d9
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-05-01 02:46:42 +0300

    Add files via upload

---
+++ LAB3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib> // для функції rand()
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+int i, j, min_idx;
+for (i = 0; i < n - 1; i++) {
+min_idx = i;
+for (j = i + 1; j < n; j++)
+if (arr[j] < arr[min_idx])
+min_idx = j;
+swap(arr[min_idx], arr[i]);
+}
+}
+
+int main() {
+for (int i = 0; i < 1099999; i++) {
+int arr[100];
+for (int i = 0; i < 100; i++) {
+arr[i] = rand() % 100 + 1;
+}
+int n = sizeof(arr) / sizeof(arr[0]);
+selectionSort(arr, n);
+}
+cout << "OK";
+return 0;
+}
