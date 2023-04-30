605a21c427681d96497142ea5e552e5fb7a74807
Dima <62839782+Dimutriy-web@users.noreply.github.com>
2023-04-30 20:06:30 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,8 +6,8 @@ for j in "${arr[@]}"; do
   for i in {1..3}; do
     x1=$(date +%M)
     x2=$(date +%S)
-    echo icpc -std=c++17 -O$i -$j LAB3.cpp -o go-$x1-$x2
-    icpc -std=c++17 -O$i -$j LAB3.cpp -o go-$x1-$x2
+    echo icpc -std=c++17 -O$i $j LAB3.cpp -o go-$x1-$x2
+    icpc -std=c++17 -O$i $j LAB3.cpp -o go-$x1-$x2
     echo "$j $i:"
     time ./go-$x1-$x2
   done 
